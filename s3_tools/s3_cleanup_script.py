b0cf2738fdb433b1b46570dff50d1d7df61a269c
pablito <pablito@amazon.com>
2019-08-05 15:06:02 -0700

    Version 1 of s3 cleanup

--- s3_tools/s3_cleanup_script.py
+++ s3_tools/s3_cleanup_script.py
@@ -1,3 +1,18 @@
+# ############################################################################
+#
+# Built this quick script to keep my sandbox account buckets clean, during
+# experimentation and testing tend to create too many buckets and data that
+# is not really required and costs money.
+#
+# The script takes a list of buckets to keep (whitelist) and removes any other
+# buckets. Use with caution, you might loose important data. Thus I only use
+# with my sandbox account.
+#
+# Version
+# 1.0 - 2019-07-05 - First Version
+#
+# ############################################################################
+
 import boto3
 import json
 
@@ -21,7 +36,8 @@ whitelist = [
     'pablo.textract.input',
     'pablo.transfer.sftp'
 ]
-mode = 'Run'
+# Use 'Run' to delete, any other value will run a simulation.
+mode = 'Dev'
 
 def main():
     print('STARTING PROCESS...')
@@ -33,7 +49,7 @@ def delete_buckets(buckets_to_ignore):
     buckets = s3.list_buckets()
     for bucket in buckets['Buckets']:
         if should_i_delete_bucket(bucket['Name'],buckets_to_ignore):
-            print (bucket['Name'] + " DELETING")
+            print (bucket['Name'] + " DELETING...")
             if mode == 'Run':
                 delete_bucket(bucket['Name'])
         else:
@@ -46,10 +62,13 @@ def should_i_delete_bucket(bucket_name, buckets_to_ignore):
     return True
 
 def delete_bucket(bucket_name):
-    s3 = boto3.client('s3')
-    bucket = s3.Bucket(bucket-name)
-    bucket.objects.all().delete()
-    return True
+    try:
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(bucket_name)
+        bucket.objects.all().delete()
+        print('DELETED')
+    except Exception as e:
+        print('ERROR: ' + str(e))
 
 if __name__ == '__main__':
     main()

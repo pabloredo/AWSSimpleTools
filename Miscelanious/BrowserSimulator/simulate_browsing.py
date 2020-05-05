eba8d6ecd2495cfcce9ed4af9102c8a23682b926
pablito <pablito@amazon.com>
2020-05-05 14:32:41 -0700

    fix index count

--- Miscelanious/BrowserSimulator/simulate_browsing.py
+++ Miscelanious/BrowserSimulator/simulate_browsing.py
@@ -27,6 +27,7 @@ def distribute_requests(request_count, distribution, delay, cloudfront_domain, s
                 total_s3 = total_s3 + page_request("https://{}.s3.amazonaws.com/{}".format(s3_bucket, image))
             else:
                 total_cf = total_cf + page_request("https://{}/{}".format(cloudfront_domain, image))
+        index += 1
     print("RESULTS: Total CF: {} Kb, Total S3: {} Kb".format(total_cf/1024, total_s3/1024))
 
 
